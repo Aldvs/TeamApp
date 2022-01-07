59809347996c11fbc717349e7b4e83f7a0f1d029
il27 <cun.i20000@gmail.com>
2022-01-07 19:07:43 +0300

    custom window design

---
+++ TeamApp/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  TeamApp
+//
+//  Created by Ильяс on 07.01.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
