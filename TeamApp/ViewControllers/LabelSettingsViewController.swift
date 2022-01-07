560ccfc2ca997079c6cda5489ac705ea4397a777
NosovaDaria <kuleshovadasha@gmail.com>
2022-01-08 01:52:20 +0300

    Set LabelSettingsViewController

--- TeamApp/ViewControllers/LabelSettingsViewController.swift
+++ TeamApp/ViewControllers/LabelSettingsViewController.swift
@@ -8,10 +8,101 @@
 import UIKit
 
 class LabelSettingsViewController: UIViewController {
-
+  
+  @IBOutlet var redSwitch: UISwitch!
+  @IBOutlet var orangeSwitch: UISwitch!
+  @IBOutlet var yellowSwitch: UISwitch!
+  @IBOutlet var greenSwitch: UISwitch!
+  @IBOutlet var blueSwitch: UISwitch!
+  @IBOutlet var purpleSwitch: UISwitch!
+  @IBOutlet var pinkSwitch: UISwitch!
+  @IBOutlet var segmentedControl: UISegmentedControl!
+  
+  
+  var delegate: SettingsLabeleAndViewControllerDelegate!
+  var selectedColor: UIColor!
+  var backgroundColor: UIColor?
+  var fontColor: UIColor?
+  
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+  @IBAction func segmentedControlAction() {
+    switch segmentedControl.selectedSegmentIndex {
+    case 0:
+      offOtherSwitches(nil)
+    default:
+      offOtherSwitches(nil)
+    }
 
+
+  }
+  @IBAction func switchesAction(_ sender: UISwitch) {
+    offOtherSwitches(sender)
+        switch sender {
+        case redSwitch:
+          selectedColor = .red
+          redSwitch.onTintColor = .red
+        case orangeSwitch:
+          selectedColor = .orange
+          orangeSwitch.onTintColor = .orange
+        case yellowSwitch:
+          selectedColor = .yellow
+          yellowSwitch.onTintColor = .yellow
+        case greenSwitch:
+          selectedColor = .green
+          greenSwitch.onTintColor = .green
+        case blueSwitch:
+          selectedColor = .blue
+          blueSwitch.onTintColor = .blue
+        case purpleSwitch:
+          selectedColor = .purple
+          purpleSwitch.onTintColor = .purple
+        default:
+          selectedColor = .systemPink
+          pinkSwitch.onTintColor = .systemPink
+        }
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+          backgroundColor = selectedColor
+        default:
+          fontColor = selectedColor
+        }
+    }
+  
+  
+  @IBAction func saveButtonPressed() {
+    delegate?.setColors?(fontColor ?? .black, backgroundColor ?? .white)
+    dismiss(animated: true)
+  }
+  
+}
+
+extension LabelSettingsViewController {
+  private func offOtherSwitches (_ choosenSwitch: UISwitch?){
+    
+    if choosenSwitch != redSwitch {
+      redSwitch.isOn = false
+    }
+    if choosenSwitch !== orangeSwitch {
+      orangeSwitch.isOn = false
+    }
+    if choosenSwitch !== yellowSwitch {
+      yellowSwitch.isOn = false
+    }
+    if choosenSwitch !== greenSwitch {
+      greenSwitch.isOn = false
+    }
+    if choosenSwitch != blueSwitch {
+      blueSwitch.isOn = false
+    }
+    if choosenSwitch != purpleSwitch {
+      purpleSwitch.isOn = false
+    }
+    if choosenSwitch != pinkSwitch {
+      pinkSwitch.isOn = false
+    }
+  }
+  
 }
